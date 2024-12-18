2b20509eb7c0f9bdbc3f6f8b2c8657a52776c506
Prikshit js <prikshitxcode@gmail.com>
2024-12-18 11:50:36 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,33 @@
 let menubar = document.getElementById("menubar");
-let menu    = document.querySelector(".left_container");
+let menu = document.querySelector(".left_container");
 
-menubar.addEventListener("click", ()=> {
-    menu.classList.toggle("show_menu");
-    menubar.classList.toggle("menubar_move");
-})
+menubar.addEventListener("click", () => {
+  menu.classList.add("show_menu");
+});
+document.getElementById("closemenu").addEventListener("click", ()=> {
+    menu.classList.remove("show_menu");
+});
+async function main() {
+    let data = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await data.text();
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let links = div.getElementsByTagName("a");
+    // let name = div.querySelectorAll(".name");
+    for (let index = 3; index < links.length; index++) {
+      const element = links[index];
+      console.log(element.href)
+      console.log(element.innerText)
+      console.log(element.innerHTML)
+    }
+    let names = div.querySelectorAll(".name");
+    console.log(names);
+    for (let index = 1; index < names.length; index++) {
+      const element = names[index];
+      // console.log(element.href)
+      console.log(element.innerText)
+      // console.log(element.innerHTML)
+    }
+}
+main();
